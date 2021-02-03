f379ebb3e6de231b01cc9c570091e649c62fae17
WooCheol Kim <jikimee64@gmail.com>
2021-02-03 23:09:09 +0900

    [feat] 레디스 연동 후 스프링 캐시 적용(MovieService)

--- src/main/java/com/study/task/service/MovieService.java
+++ src/main/java/com/study/task/service/MovieService.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -20,7 +21,9 @@ public class MovieService {
 
     private final MovieRepository movieRepository;
 
+    @Cacheable(value = "cache::movie::query")
     public List<ResponseMovieClientDto> search(final String query){
+        log.info("MovieService search 메소드 실행");
         MovieGroup movieGroup = new MovieGroup(movieRepository.findByQuery(query));
         return movieGroup.getListOrderRating();
     }
